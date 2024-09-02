ac496a747e23763e6ccc651b85980cb19da33b99
wanghch <70200472+wanghch123@users.noreply.github.com>
2024-09-02 18:43:23 +0800

    Add metric build entry (#1)
    Add build_metric and REGISTER_METRIC for metric usage.

--- model/__init__.py
+++ model/__init__.py
@@ -3,5 +3,6 @@ from .build import MODEL_REGISTRY, build_model
 from .checkpoint import load_pretrained
 
 from .criterion import *
+from .metric import *
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
